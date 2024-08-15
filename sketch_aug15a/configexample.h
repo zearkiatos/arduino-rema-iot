15d6dfd38ff246d8665222adbc49d7f64ab2f014
zearkiatos <caprilespe@outlook.com>
2024-08-15 16:57:44 -0400

    Connecting to the wifi

---
+++ sketch_aug15a/configexample.h
@@ -0,0 +1,8 @@
+// config.h
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define SSID ""
+#define PASSWORD ""
+
+#endif
--- sketch_aug15a/sketch_aug15a.ino
+++ sketch_aug15a/sketch_aug15a.ino
@@ -1,39 +1,111 @@
+#include <ESP8266WiFi.h>
 #include "DHT.h"
+#include "config.h"
 #define DHTTYPE DHT11  // DHT 11
 
-#define dht_dpin 4
-DHT dht(dht_dpin, DHTTYPE);
+const char *ssid = SSID;
+const char *password = PASSWORD;
 
-#define LED D1  // LED
-int ValueRead = 2;
-int myflag = 0;
+#define LED D1
+uint8_t DHTPin = 4;
+DHT dht(DHTPin, DHTTYPE);
+
+float Temperature;
+float Humidity;
+
+WiFiServer server(80);
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  delay(10);
   pinMode(LED, OUTPUT);
   digitalWrite(LED, LOW);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  server.begin();
+  Serial.println("Server started");
+
+  Serial.print("Use this URL to connect: ");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+
+
   dht.begin();
 }
 
+
 void loop() {
-  if (Serial.available()) {
-    ValueRead = Serial.parseInt();
+  Temperature = dht.readTemperature();  // Gets the values of the temperature
+  Humidity = dht.readHumidity();        // Gets the values of the humidity
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
   }
-  if (((ValueRead == 1 && myflag == 0) || myflag == 1) & !(ValueRead == 2 && myflag == 1)) {
+
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while (!client.available()) {
+    delay(1);
+  }
+
+  // Read the first line of the request
+  String request = client.readStringUntil('\r');
+  Serial.println(request);
+  client.flush();
+
+  // Match the request
+
+  int value = HIGH;
+  if (request.indexOf("/LED=ON") != -1) {
     digitalWrite(LED, HIGH);
-    Serial.println("Turn On");
-    myflag = 1;
-  } else {
-    digitalWrite(LED, LOW);
-    Serial.println("Turn Off");
-    myflag = 0;
+    value = HIGH;
   }
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-  Serial.print("Current humidity = ");
-  Serial.print(h);
-  Serial.print("%  ");
-  Serial.print("temperature = ");
-  Serial.print(t);
-  Serial.println("C  ");
+  if (request.indexOf("/LED=OFF") != -1) {
+    digitalWrite(LED, LOW);
+    value = LOW;
+  }
+
+  // Return the response
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println("");  //  do not forget this one
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+
+  client.print("LED is now: ");
+
+  if (value == LOW) {
+    client.print("Off");
+  } else {
+    client.print("On");
+  }
+  client.println("<br><br>");
+  client.println("<a href=\"/LED=ON\"\"><button>Turn On </button></a>");
+  client.println("<a href=\"/LED=OFF\"\"><button>Turn Off </button></a><br />");
+  client.println("</html>");
+
+  client.println("<br><br>");
+  client.print("Temperature: ");
+  client.print(Temperature);
+  client.print("C");
+  client.println("<br><br>");
+  client.print("Humidity: ");
+  client.print(Humidity);
+  client.print("%");
+
+  delay(1);
+  Serial.println("Client disconnected");
+  Serial.println("");
 }
